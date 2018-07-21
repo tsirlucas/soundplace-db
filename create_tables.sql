8d3ab0332712e8d3a7c11b7067c02c1355a6036a
tsirlucas <tsirlucas@gmail.com>
2018-07-21 17:19:01 -0300

    feat(spotifyAuth): add spotify auth table

--- create_tables.sql
+++ create_tables.sql
@@ -37,6 +37,13 @@ CREATE TABLE soundplace.track_data
   album_id text REFERENCES soundplace.album_data (id) NOT NULL
 );
 
+CREATE TABLE soundplace.spotify_auth
+(
+  user_id text REFERENCES soundplace.user_data (id) NOT NULL,
+  access_token text NOT NULL,
+  refresh_token text NOT NULL
+);
+
 -- RELATIONSHIP
 
 CREATE TABLE soundplace.playlist_track
