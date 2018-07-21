41207e0a9d07a987dae309195576a2874e31d492
tsirlucas <tsirlucas@gmail.com>
2018-07-21 11:18:29 -0300

    fix(tables): fix tables relationships

--- create_tables.sql
+++ create_tables.sql
@@ -12,7 +12,7 @@ CREATE TABLE soundplace.playlist_data
   id text PRIMARY KEY,
   name text NOT NULL,
   cover text,
-  user_id text REFERENCES soundplace.user_data (id)
+  user_id text REFERENCES soundplace.user_data (id) NOT NULL
 );
 
 CREATE TABLE soundplace.album_data
@@ -32,29 +32,16 @@ CREATE TABLE soundplace.track_data
 (
   id text PRIMARY KEY,
   name text NOT NULL,
-  album text REFERENCES soundplace.album_data (id),
-  duration numeric NOT NULL
+  duration numeric NOT NULL,
+  artist_id text REFERENCES soundplace.artist_data (id) NOT NULL,
+  album_id text REFERENCES soundplace.album_data (id) NOT NULL
 );
 
 -- RELATIONSHIP
 
 CREATE TABLE soundplace.playlist_track
 (
-  playlist_id text REFERENCES soundplace.playlist_data (id),
-  track_id text REFERENCES soundplace.track_data (id),
+  playlist_id text REFERENCES soundplace.playlist_data (id) NOT NULL,
+  track_id text REFERENCES soundplace.track_data (id) NOT NULL,
   CONSTRAINT playlist_track_pkey PRIMARY KEY (playlist_id, track_id)
-);
-
-CREATE TABLE soundplace.album_artist
-(
-  album_id text REFERENCES soundplace.album_data (id),
-  artist_id text REFERENCES soundplace.artist_data (id),
-  CONSTRAINT album_artist_pkey PRIMARY KEY (album_id, artist_id)
-);
-
-CREATE TABLE soundplace.track_artist
-(
-  track_id text REFERENCES soundplace.track_data (id),
-  artist_id text REFERENCES soundplace.artist_data (id),
-  CONSTRAINT artist_artist_pkey PRIMARY KEY (track_id, artist_id)
 );
